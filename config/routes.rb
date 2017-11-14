6c09fc190eaa17b1ff55aa78ce7466e83f7d0f60
Paul Yun <paul.a.yun@gmail.com>
2017-11-13 23:21:44 -0500

    Add styles, navbar, flash message, index page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  authenticated :user do
+    root 'home#welcome', as: 'authenticated_root'
+  end
+
+  root 'home#index'
+
+  # catchall
+  match '*path', to: redirect('/'), via: :all
 end
